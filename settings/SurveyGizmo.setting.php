47a1c6f9a7a2a960ec13a7de93b8ffaf9bf7c668
Andrew Cormick-Dockery <andrew.cormick-dockery@greens.org.au>
2017-06-20 00:57:27 +0000

    Align git repo with current production status

---
+++ settings/SurveyGizmo.setting.php
@@ -0,0 +1,37 @@
+<?php
+return array(
+  'surveygizmo_api_key' => array(
+    'group_name' => 'SurveyGizmo Settings',
+    'group' => 'surveygizmo',
+    'name' => 'surveygizmo_api_key',
+    'type' => 'String',
+    'add' => '4.7',
+    'is_domain' => 0,
+    'is_contact' => 0,
+    'description' => 'SurveyGizmo API Key',
+    'title' =>  'SurveyGizmo API Key',
+    'help_text' => 'To obtain, SurveyGizmo administrative users can go to Account > Account Settings > Manage API and click Create an API Key.',
+    'html_type' => 'Text',
+    'html_attributes' => array(
+      'size' => 50,
+    ),
+    'quick_form_type' => 'Element',
+  ),
+  'surveygizmo_api_secret' => array(
+    'group_name' => 'SurveyGizmo Settings',
+    'group' => 'surveygizmo',
+    'name' => 'surveygizmo_api_secret',
+    'type' => 'String',
+    'add' => '4.7',
+    'is_domain' => 0,
+    'is_contact' => 0,
+    'description' => 'SurveyGizo API Secret Key',
+    'title' => 'SurveyGizo API Secret Key',
+    'help_text' => 'To obtain, SurveyGizmo administrative users can go to Account > Account Settings > Manage API and click Create an API Key.',
+    'html_type' => 'Text',
+    'html_attributes' => array(
+      'size' => 50,
+    ),
+    'quick_form_type' => 'Element',
+  ),
+ );
